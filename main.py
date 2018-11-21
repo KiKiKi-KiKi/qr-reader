216a9737a4f63c636b7fac4014b2aeb5ecb832f0
KiKiKi <chaika.design@gmail.com>
2018-11-21 19:53:40 +0900

    fix typo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, Response
 from pyzbar import pyzbar
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from detetime import datetime
+from datetime import datetime
 
 import numpy as np
 import cv2
